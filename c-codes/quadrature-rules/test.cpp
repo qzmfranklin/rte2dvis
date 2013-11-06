8ecd4918a285ff7f42e73fecc41162e072450913
Zhongming Qu <qzmfrank@umich.edu>
2013-11-06 10:21:41 -0500

    20131106 10:21:39

--- c-codes/quadrature-rules/test.cpp
+++ c-codes/quadrature-rules/test.cpp
@@ -1,21 +1,27 @@
+#include "DunavantRule.h"
 #include "WandzuraRule.h"
+#include "GaussQuadratures.h"
 /*****************************************************************************/
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
 /*****************************************************************************/
+using namespace QuadratureRules;
 int main(int argc, char const* argv[]);
 void test01(int n);
+void test02(int n);
+void test03(int n);
 void print_rule( double *xy, double *w, int order_num );
 /*****************************************************************************/
 int main(int argc, char const* argv[])
 { 
 	test01(100);
+	test02(100);
+	test03(100);
 	return 0;
-}
-
+} 
 
 void test01( int n ){
-	n = MIN(n,6);
+	n = MIN(n,20);
 	n = MAX(n,1);
 	int rule;
 	int order_num;
@@ -24,6 +30,34 @@ void test01( int n ){
 	for (rule = 1; rule <= n; rule++) {
 		gDunavantRule.Generate( rule, &xy, &w, &order_num );
 		print_rule( xy, w, order_num ); 
+	}
+}
+
+void test02( int n ){
+	n = MIN(n,6);
+	n = MAX(n,1);
+	int rule;
+	int order_num;
+	double *xy;
+	double *w;
+	for (rule = 1; rule <= n; rule++) {
+		gWandzuraRule.Generate( rule, &xy, &w, &order_num );
+		print_rule( xy, w, order_num ); 
+	}
+}
+
+void test03( int n ) {
+	printf("	Test the Legendre rules.\n");
+	printf("		A = -1.0\n");
+	printf("		B = +1.0\n");
+	n = MIN(n,50);
+	n = MAX(n,1);
+	int order;
+	double *x;
+	double *w;
+	for (order = 1; order <= n; order++) {
+		gGaussQuadratures.Generate( &x, &w, order, -1.0, 1.0 );
+		print_rule( x, w, order ); 
 	}
 }
 
