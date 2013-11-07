5a89c9f1ec03081f4cb5297ee31313df0a0735b9
Zhongming Qu <qzmfrank@umich.edu>
2013-11-06 22:29:53 -0500

    20131106 22:29:55

--- c-codes/makevars.mk
+++ c-codes/makevars.mk
@@ -1,33 +1,31 @@
-# The template Makefile header in subdirectories:
-##NOINLINEOPT := True 		# Disable automatic function inlining.
-##UNIVERSAL_BINARY := True	# Produce universal binary -fPIC.
-## Suppress warning messages about deprecated or antiquated headers.
-#CFLAGS 	:= -Wno-deprecated	# additional options for the directory
-#include ../makevars.mk
-## The MKL library is already linked. Need not to be specified.
-##INCS	:= ${INCS} 
-##LIBS	:= ${LIBS}
 ###############################################################################
-# Header file and binary file directories 
-INCLUDE	:= ./include
-BIN	:= ./bin 
-###############################################################################
-ifdef NOINLINE
-	NOINLINEOPT := -fno-inline-functions
-endif 
-ifdef UNIVERSAL_BINARY
-	FPIC := -fPIC
-endif 
+HDRFILES:= $(shell find . -type f -name "*.h") # Header
+TSTFILES:= $(shell find . -type f -name "test.cpp") # Test
+CFILES	:= $(shell find . -type f -name "*.c") # C Source
+CPPFILES:= $(filter-out ${TSTFILES}, $(shell find . -type f -name "*.cpp")) # CPP Source
+SRCFILES:= ${CFILES} ${CPPFILES} # Source
+OBJFILES:= ${CFILES:%.c=%.o} ${CPPFILES:%.cpp=%.o} # Object
+DEPFILES:= ${OBJFILES:%.o=%.d} # Dependency
+ASMFILES:= ${OBJFILES:%.o=%.s} # Assembly
+DOCFILES:= # Documentation
+EXPFILES:= # Example
+MANFILES:= # Manual
+AUXFILES:= ${DOCFILES} ${EXPFILES} ${MANFILES} # Auxilliary
+ALLFILES:= ${SRCFILES} ${HDRFILES} ${AUXFILES} # All Files for Distribution
 ###############################################################################
 CC	:= icc
 CXX	:= icpc
-CFLAGS 	:= ${CFLAGS} -Wall -O3				\
-	${FPIC}						\
-	${NOINLINEOPT}					\
+CFLAGS 	:= -O3						\
+	-Wall						\
+	-Wno-deprecated					\
 	-prec-div -no-ftz				\
-	-iquote ${INCLUDE}				\
 	-restrict
-CFLAGSXX:= ${CFLAGSXX} ${CFLAGS}
+	# 						\
+	-fPIC						\
+	-fno-inline-functions				\
+	-iquote ${INCLUDES}				\
+
+CFLAGSXX:= ${CFLAGS}
 # Intel Math Kernel Library
 MKL_INCS:= 
 MKL_LIBS:= -mkl
@@ -41,90 +39,54 @@ MLL_INCS:= -I/Applications/Mathematica.app/SystemFiles/IncludeFiles/C/
 MLL_LIBS:= -L/Applications/Mathematica.app/SystemFiles/Libraries/MacOSX-x86-64/
 # The default empty include directories and 
 # linking libraries for specific directories
-INCS	:= ${MKL_INCS}
+INCLUDES:= $(foreach dir, $(shell find . -type d -name "include"), -I${dir})
+INCS	:= ${INCLUDES} ${MKL_INCS}
 LIBS	:= ${MKL_LIBS}
-###############################################################################
-HDRFILES := $(shell find . -type f -name "*.h") $(shell find . -type f -name "*.h")
-TSTFILES := $(shell find . -type f -name "test.cpp")
-CFILES	 := $(shell find . -type f -name "*.c")
-CPPFILES := $(filter-out ${TSTFILES}, $(shell find . -type f -name "*.cpp"))
-SRCFILES := ${CFILES} ${CPPFILES}
-OBJFILES := ${CFILES:%.c=%.o} ${CPPFILES:%.cpp=%.o}
-DEPFILES := ${CFILES:%.c=%.d} ${CPPFILES:%.cpp=%.d}
-AUXFILES := 
-ALLFILES := ${SRCFILES} ${HDRFILES} ${AUXFILES}
 ###############################################################################
 # Colorful echo!
 NONE		:=\033[00m 
 RED		:=\033[01;31m
+BLUE		:=\033[00;34m
 GREEN		:=\033[01;32m
 YELLOW		:=\033[01;33m
 PURPLE		:=\033[01;35m
 CYAN		:=\033[01;36m
+LIGHTGRAY	:=\033[00;37m
+LRED		:=\033[01;31m
+LGREEN		:=\033[01;32m
+LYELLOW		:=\033[01;33m
+LBLUE		:=\033[01;34m
+LPURPLE		:=\033[01;35m
+LCYAN		:=\033[01;36m
 WHITE		:=\033[01;37m
 BOLD		:=\033[1m
 UNDERLINE	:=\033[4m
-###############################################################################
-.PHONY: all 						\
-	clean cleanx cleanxx 				\
-	dist						\
-	check						\
-	test						\
-	install uninstall				\
-	todolist
-
-all: ${OBJFILES}
-	@make clean
-clean:
-	@rm -rf *_tm.c *.d*
-cleanx: 
-	@rm -rf *.s DEBUG/* OUPUT/*
-cleanxx: clean
-	@rm -rf *.dylib *.so *.a *.exe a.out *.o
-dist:
-	@tar czf "${CYAN}pdclib.tgz${NONE}" ${ALLFILES}
-test: ${TSTFILES:%.cpp=%.exe}
-	@echo Running "${RED}$<${NONE}"...
-	@./$< > DEBUG/test.txt
-	@echo Debug information is in "${GREEN}DEBUG/test.txt${NONE}".
-check:
-	@echo ${TSTFILES}
-	@echo ${SRCFILES}
-	@echo ${OBJFILES}
-install: all
-	mv ${OBJFILES} ${BIN}
-todolist:
-	-@for file in ${ALLFILES:Makefile=};		\
-	do fgrep -H -e TODO -e FIXME -e UNFINISHED	\
-	$$file;	done; true 
 ############################################################################### 
 .SUFFIXES:
 .SUFFIXES: .tm .c .cpp .o .exe .s .d
 # C sources are 
 %.o: %.c
 	@echo Compiling "${PURPLE}$@${NONE}"...
-	@${CC} -c $< -MD -MP ${CFLAGS} ${INCS}
+	@${CC} -c $< -o $@ -MD -MP ${CFLAGS} ${INCS}
 %.s: %.c 
-	@echo Generating source-commented assembly list $@...
-	@${CC} -S $< ${CFLAGS} -fsource-asm ${INCS}
+	@echo Generating source-commented assembly list "${LRED}$@${NONE}"...
+	@${CC} -S $< -o $@ ${CFLAGS} -fsource-asm ${INCS}
 
 # CXX sources
 %.o: %.cpp
 	@echo Compiling "${PURPLE}$@${NONE}"...
-	@${CXX} -c $< -MD -MP ${CFLAGSXX} ${INCS}
+	@${CXX} -c $< -o $@ -MD -MP ${CFLAGSXX} ${INCS}
 %.s: %.cpp
-	@echo Generating source-commented assembly list $@...
-	@${CXX} -S $< ${CFLAGSXX} -fsource-asm ${INCS} 
+	@echo Generating source-commented assembly list "${LRED}$@${NONE}"...
+	@${CXX} -S $< -o $@ ${CFLAGSXX} -fsource-asm ${INCS} 
 
 # CXX linking at the top level
 %.exe: %.o 
 	@echo Linking executable "${RED}$@${NONE}"...
 	@${CXX} ${filter %.o,$^} -o $@ ${INCS} ${LIBS} 
 %.dylib: %.o
-	@echo Generating source-commented assembly list $@...
 	@echo Linking dynamic library "${RED}$@${NONE}"...
 	@${CXX} -dynamiclib ${filter %.o,$^} -o $@ ${INCS} ${LIBS}
 %.a: %.o
 	@echo Linking static library "${RED}$@${NONE}"...
-	@${CXX} -staticlib ${filter %.o,$^} -o $@ ${INCS} ${LIBS}
-
+	@${CXX} -staticlib ${filter %.o,$^} -o $@ ${INCS} ${LIBS} 
