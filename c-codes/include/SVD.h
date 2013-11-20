3f7a4fbce330b6cfd8472bd6042709f2d1c2b9ef
Zhongming Qu <qzmfrank@umich.edu>
2013-11-20 16:28:58 -0500

    20131120 16:28:59

--- c-codes/include/SVD.h
+++ c-codes/include/SVD.h
@@ -3,83 +3,123 @@
 #include <stack>
 #include <mkl.h>
 #include <cassert>
+#define MALLOC_ALIGNMENT 64
+
 class dSVD {
 	/*
-	 * Calling LAPACK subroutines to compute SVD
+	 * Calling LAPACK subroutines to compute SVD.
 	 * 	DOUBLE PRECISION
 	 * Full Col-Major storage:
 	 * 	LAPACK_COL_MAJOR
+	 * 
+	 * Assumed that A, U and VT are packed:
+	 * 	LDA = M, LDU = M, LDVT = N
+	 * where
+	 *	 	A = U S VT
+	 * 	A = M*N
+	 * 	U = M*M
+	 * 	VT= N*N
+	 * 	S = MIN(M,N)
 	 */
+	public:
+		int		m;
+		int		n;
+		double*		a;
+		int		info;
 	private:
+		int		lda; 
+		int		ldu;
+		int		ldvt;
+		double*		work;
+		int		lwork;
 		std::stack<double*>	fs;
 		std::stack<double*>	fu;
 		std::stack<double*>	fvt;
-		int	query_flag;	// Whether the query for workspace 
-					// is made? 1=Yes, 0=No.
+		enum EQueryType	{
+			kNone		= 0,
+			kSingularValue	= 101, // only compute singular values
+			kAll 		= 102// compute U and VT as well
+		} query_flag;
+		enum EResetType {
+			kReset,		// is reset
+			kSet		// is set
+		} reset_flag;		
 	public:
-		double*	a;
-		int	m;
-		int	n;
-		int	lda;
-	public:
-		dSVD(): a(NULL), m(0), n(0), lda(0), query_flag(0) {}
+		dSVD():	m(0), n(0), 
+			a(NULL),
+			info(0), 
+			lda(0), ldu(0), ldvt(0), 
+			work(NULL), lwork(0), 
+			query_flag(kNone),
+			reset_flag(kReset) {}
+
 		~dSVD() { Free(); }; 
 
-		void Set( double* A, int M, int N, int LDA ) {
-			memcpy( a, A, sizeof(A) );
-			m=M; n=N; lda=LDA; query_flag=0; 
+		void Set( int M, int N, double* A ) {
+			m=M; 
+			n=N; 
+			a=A; 
+			lda=m; 
+			ldu=m; 
+			ldvt=n;
+
+			if (query_flag!=kNone) 
+				mkl_free(work); 
+			query_flag=kNone; 
+			reset_flag=kSet;
 		}
 
+		void Reset() { reset_flag=kReset; }
 
-		/* 
-		 * Only computes the singular value list.
-		 * 	a)	The double *s must NOT be allocated 
-		 * 	beforehand. This function allocates memory 
-		 * 	for double *s. If you use the global instance 
-		 * 	gdSVD, there is NO need to manually free the 
-		 * 	memory allocated in this function. All 
-		 * 	memories allocated by global object will be 
-		 * 	released automatically upon destruction.
-		 * 	b)	The LAPACK subroutine assumes the
-		 * 	leading dimension of s =1.
-		 * 	c) 	If return value == 0, success. Other-
-		 * 	wise, failure.
-		 * 	d)	int *len stores the length of s.
-		 */
-		int SingularValueList( double **s, int *len );
-
-		/* 
-		 * Only computes the singular value list.
-		 * 	a)	The double *s must be allocated beforehand.
-		 * 	b)	The LAPACK subroutine assumes the 
-		 * 	leading dimension of s =1.
-		 * 	c) 	If return value == 0, success. Other-
-		 * 	wise, failure.
-		 */
-		int SingularValueList( double *s );
-
-		/* 
-		 * 1.	Computes all singular values, store them in 
-		 * double *s_temp.
-		 * 2.	Truncate the singular value list to the relative 
-		 * error of eps. If, however, eps==0.0 (default), no 
-		 * truncation will be made. Assume length(s_temp)=N.
-		 * 3.	Return the first N left and right vectors in 
-		 * u and vt, respectively. Return the first N singualr 
-		 * values in s.
-		 */
-		//void SingularValueDecomposition( double *s, 
-						//double *u,  int ldu, 
-						//double *vt, int ldvt,
-						//double eps=0.0 ); 
-	private:
 		/*
 		 * Query for workspace.
+		 * Return values:
+		 * 	-1	Matrix A not set
+		 * 	0	Already queried
+		 * 	>0	Query Status
 		 */
-		void QueryWorkspace();
-
+		int QueryWorkspace( 
+				double *restrict s,
+				double *restrict u =NULL,
+				double *restrict vt=NULL );
 
 		/*
+		 * Only get the singular value list. 
+		 * Destroy the original matrix A.
+		 */
+		int SingularValueListX( double *s );
+		
+		/*
+		 * Return the sigular value list and the U and VT.
+		 * Destroy the original matix A.
+		 */
+		int SingularValueDecompositionX( 
+				double *restrict s,
+				double *restrict u, 
+				double *restrict vt );
+
+		/*
+		 * Only get the singular value list. 
+		 * Not destroy the original matrix A.
+		 */
+		int SingularValueList( double *s );
+		
+		/*
+		 * Return the sigular value list and the U and VT.
+		 * Not destroy the original matrix A.
+		 */
+		int SingularValueDecomposition( 
+				double *restrict s,
+				double *restrict u, 
+				double *restrict vt );
+
+	private:
+		void Copy( int m, int n, double* restrict a, double* restrict b ) {
+			mkl_domatcopy ( 'C', 'N', m, n, 1.0, a, m, b, m );
+		}
+
+		/*
+		 * Free memories.
 		 * The design here is to release the programmer from
 		 * having to constantly worry about memory management.
 		 * Please just use the global object, gdSVD, to do
@@ -89,28 +129,8 @@ class dSVD {
 		 * neccessary, one can call gdSVD.Free() to manually
 		 * free all existing memories allocated by gdSVD.
 		 */
-		void Free() {
-			printf("dSVD::Free()\n");
-			printf("	|fs.size() = %lu \n",fs.size());
-			while( !fs.empty() ) {
-				//printf("poping fs.top()\n");
-				mkl_free(fs.top());
-				fs.pop();
-			}
-			printf("	|fu.size() = %lu \n",fu.size());
-			while( !fu.empty() ) {
-				//printf("poping fu.top()\n");
-				mkl_free(fu.top());
-				fu.pop();
-			}
-			printf("	|fvt.size() = %lu \n",fvt.size());
-			while( !fvt.empty() ) {
-				//printf("poping fvt.top()\n");
-				mkl_free(fvt.top());
-				fvt.pop();
-			}
-		}
-}; 
+		void Free(); 
+};
 
 /*
  * Global instance of dSVD. Used to manage memories with the help of 
--- c-codes/src/SVD/SVD.cpp
+++ c-codes/src/SVD/SVD.cpp
@@ -12,21 +12,73 @@ void dgesvd( const char* jobu, const char* jobvt, const int* m,
              double* u, const int* ldu, double* vt, const int* ldvt,
              double* work, const int* lwork, int* info );
 /*******************************************************************************/
-//void dSVD::QueryWorkspace(const char* jobu, const char* jobvt) {
-	//// TODO
-//}
+int dSVD::QueryWorkspace( 
+		double *restrict s,
+		double *restrict u, 
+		double *restrict vt ) {
+	//printf("dSVD::QueryWorkspace()\n");
 
-int dSVD::SingularValueList( double *s ) { 
-	printf("dSVD::SingularValueList(double *s)\n");
-	// TODO
+	if (  reset_flag == kReset  ) {
+		fprintf(stderr, "A matrix is not set.\n");
+		return -1;
+	}
 
-	//return info;
-	return 0;
+	enum EQueryType type;
+	if ( !(u||vt) )
+		type = kSingularValue;
+	else
+		type = kAll;
+
+	if (  type == query_flag  ) {
+		fprintf(stderr, "Already queried.\n");
+		return 0; 
+	}
+
+	//TODO
+	char *_jobu="N", *_jobvt="N";
+	double _work[1]={0.0};
+	int _ldu=1, _ldvt=1;
+	mkl_free(work);	// Free previous workspace.
+	switch (  type  ) {
+		case kSingularValue: 
+			lwork=-1;
+			// Query for optimal workspace size.
+			//dgesvd(_jobu,_jobvt,&m,&n,a,&lda,s,u,&ldu,vt,&ldvt,_work,&lwork,info);
+			assert(info==0);
+			lwork = (int) *_work;
+			work = (double*) mkl_malloc( lwork * sizeof(double), MALLOC_ALIGNMENT );
+			assert(work);
+			query_flag = type;
+			break;
+		case kAll: 
+			break;
+		default:
+			fprintf(stderr,"\tArgument 'type' is not one of kSingularValue, kAll.\n");
+			return NULL;
+			break;
+	}
+
+	return query_flag;
 }
 
-//void dSVD::SingularValueDecomposition( 	double *s, 
-					//double *u,  int ldu, 
-					//double *vt, int ldvt,
-					//double eps ) {
-					//// TODO
-//}
+void dSVD::Free() {
+	printf("dSVD::Free()\n");
+	printf("	|fs.size() = %lu \n",fs.size());
+	while( !fs.empty() ) {
+		//printf("poping fs.top()\n");
+		mkl_free(fs.top());
+		fs.pop();
+	}
+	printf("	|fu.size() = %lu \n",fu.size());
+	while( !fu.empty() ) {
+		//printf("poping fu.top()\n");
+		mkl_free(fu.top());
+		fu.pop();
+	}
+	printf("	|fvt.size() = %lu \n",fvt.size());
+	while( !fvt.empty() ) {
+		//printf("poping fvt.top()\n");
+		mkl_free(fvt.top());
+		fvt.pop();
+	}
+}
