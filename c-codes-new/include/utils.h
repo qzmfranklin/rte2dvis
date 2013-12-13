ebc68ed02955eb2df54fc62efc082b73d84aff8c
Zhongming Qu <qzmfrank@umich.edu>
2013-12-13 13:49:18 -0500

    Delete dir in CPPFILES

--- c-codes-new/include/utils.h
+++ c-codes-new/include/utils.h
@@ -6,141 +6,25 @@
 #include "StatVector.h"
 #include "TimeStamp.h"
 /******************************************************************************/
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/*
- * Output a matrix to a file.
- */
-void matrix_out(const char* fname, const int m, const int n, double *restrict v,
-		int lda=0);
-
-/*
- * Flush an array (either matrix or vector).
- */
-void array_flush(double *v, int size);
-
-/*
- * Solves A.x=rhs.
- */
-void solve2x2(double *restrict A, double *restrict rhs, double *restrict x);
-
-/*
- * Verify directory (create).
- */
-void verify_dir(const char *dir);
-
-/*
- * Link stdout to a file.
- */
-void link_stdout(const char *fname);
-
-/*
- * Unlink and restore stdout redirected by link_stdout.
- */
-void unlink_stdout();
-
-/*
- * Initialize a vector randomly. Max=10.0.
- */
-void init_vector(int n, double* v, int incx=1);
-
-/*
- * Initialize a matrix randomly. Max=10.0.
- */
-void init_matrix(int m, int n, double* a, int lda=0);
-
-/*
- * Print a vector to stdout.
- */
-void print_vector(const char* desc, const int n, double* v, int incx=1);
-
-/*
- * Print a matrix to stdout.
- */
-void print_matrix(const char* desc, const int m, const int n, 
-		double *a, int lda=0);
-/*
- * Link std::cout to a file.
- */
-void link_cout(const char *fname);
-
-/*
- * Unlink and restore std::cout redirected by link_cout.
- */
-void unlink_cout();
-
-/*
- * Import a matrix from a file.
- */
-void array_in(const char* fname, const int m, const int n, double *restrict a,
-		const int lda);
-
-/*
- * Export a matrix to a file.
- * If fname==NULL, use stdout.
- */
+void array_abs(double *v, int n); 
+double array_max(double *v, int n); 
+void array_diff(double *restrict v, double *restrict w, int n); 
+void array_copy(double *restrict v, double *restrict w, int n); 
 void array_out(const char* fname, const int m, const int n, double *restrict a,
-		const int lda);
-
-/*
- * Flush an array (either matrix or vector).
- */
-void array_flush(double *v, int size);
-
-/*
- * Solves A.x=rhs.
- */
-void solve2x2(double *restrict A, double *restrict rhs, double *restrict x);
-
-/*
- * Verify directory (create).
- */
-void verify_dir(const char *dir);
-
-/*
- * Link cout to a file.
- */
-void link_cout(const char *fname);
-
-/*
- * Unlink and restore cout redirected by link_cout.
- */
-void unlink_cout();
-
-/*
- * Link stdout to a file.
- */
-void link_stdout(const char *fname);
-
-/*
- * Unlink and restore stdout redirected by link_stdout.
- */
-void unlink_stdout();
-
-/*
- * Initialize a vector randomly. Max=10.0.
- */
+		const int lda); 
+void array_in(double *v, int size,  const char* fname); 
+void array_flush(double *v, int size); 
+void solve2x2(double *restrict A, double *restrict rhs, double *restrict x); 
+void verify_dir(const char *dir); 
+void link_cout(const char *fname); 
+void unlink_cout(); 
+void link_stdout(const char *fname); 
+void unlink_stdout(); 
+void box_file(const char* fname, const char*mesg); 
+void print_proc_status(const char* mesg); 
 void init_vector(int n, double* v, int incx);
-
-/*
- * Initialize a matrix randomly. Max=10.0.
- */
-void init_matrix(int m, int n, double* a, int lda);
-
-/*
- * Print a vector to stdout.
- */
-void print_vector(const char* desc, const int n, double* v, int incx);
-
-/*
- * Print a matrix to stdout.
- */
-void print_matrix(const char* desc, const int m, const int n, 
-		double *a, int lda);
-#ifdef __cplusplus
-}
-#endif
+void init_matrix(int m, int n, double* a, int lda); 
+void print_vector(const char* desc, const int n, double* v, int incx); 
+void print_matrix(const char* desc, const int m, const int n, double *a, int lda); 
 /******************************************************************************/
 #endif
--- c-codes-new/src/utils/utils.cpp
+++ c-codes-new/src/utils/utils.cpp
@@ -2,6 +2,7 @@
 #include <emmintrin.h>
 #include <cmath>
 #include <cstdio>
+#include <fstream>
 #include <iostream>
 #include <iomanip>
 #include <sys/types.h>
@@ -28,12 +29,14 @@
 /******************************************************************************/ 
 using namespace std;
 
-void array_abs(double *v, int n){
+void array_abs(double *v, int n)
+{
 	for(int i=0; i < n; i++)
 		v[i] = fabs(v[i]);
 }
 
-double array_max(double *v, int n){
+double array_max(double *v, int n)
+{
 	double ans = 0;
 	for(int i=0; i < n; i++)
 		if(fabs(v[i])> ans)
@@ -41,27 +44,36 @@ double array_max(double *v, int n){
 	return ans;
 }
 
-
-void array_diff(double *restrict v, double *restrict w, int n){
+void array_diff(double *restrict v, double *restrict w, int n)
+{
 	for(int i=0; i < n; i++)
 		v[i] -= w[i];
 }
 
-void array_copy(double *restrict v, double *restrict w, int n){
+void array_copy(double *restrict v, double *restrict w, int n)
+{
 	for(int i=0; i < n; i++)
 		w[i] = v[i];
 }
 
-void array_out(double *v, int m, int n, const char* fname){
+void array_out(const char* fname, const int m, const int n, double *restrict a,
+		const int lda)
+{
+	assrt(lda>=m);
+
 	FILE *fp;
-	if(fname == NULL)
+	if(fname == NULL) {
 		fp = stdout;
-	else
+		fprintf(stderr, "Export array to STDOUT\n");
+	}
+	else {
 		fp = fopen(fname, "w");
+		fprintf(stderr, "Export array to %s\n",fname);
+	}
 
 	for(int i=0; i < m; i++){
 		for(int j=0; j < n; j++)
-			fprintf(fp, "%f  ", v[i+j*m]);
+			fprintf(fp, "%f  ", a[i+j*lda]);
 		fprintf(fp, "\n");
 	}
 
@@ -71,34 +83,8 @@ void array_out(double *v, int m, int n, const char* fname){
 	}
 }
 
-void array_out(double *v, int m, int n, int lda, const char *fname){
-	FILE *fp;
-	if(fname == NULL)
-		fp = stdout;
-	else
-		fp = fopen(fname, "w");
-
-	for(int i=0; i < m; i++){
-		for(int j=0; j < n; j++)
-			fprintf(fp, "%f  ", v[i+j*lda]);
-		fprintf(fp, "\n");
-	}
-
-	if(fname != NULL){
-		int rval = fclose(fp);
-		assrt(rval !=  -1);
-	}
-}
-
-void array_show(double *v, int n, const char* mesg){
-	cout<<"\t\t\t\t"<<mesg<<endl;
-	cout<<scientific<<setprecision(15);
-	for(int i=0; i < n; i++)
-		cout<<"\t\t"<<setw(3)<<i<<": "<<v[i]<<endl;
-	cout<<endl;
-}
-
-void array_in(double *v, int size,  const char* fname){
+void array_in(double *v, int size,  const char* fname)
+{
 	std::ifstream ifile(fname);
 	assrt(ifile.is_open());
 	for(int i=0; i < size; i++)
@@ -106,7 +92,24 @@ void array_in(double *v, int size,  const char* fname){
 	ifile.close();
 }
 
-void array_flush(double *v, int size){
+/*void array_in(const char* fname, const int m, const int n, double *restrict a,*/
+		/*const int lda)*/
+/*{*/
+	/*assrt(lda>=m); */
+
+	/*FILE *fp = fopen(fname,"r");*/
+	/*assrt(fp);*/
+	/*fprintf(stderr, "Import array from %s\n",fname);*/
+
+	/*for (int i = 0; i < m; i++)*/
+		/*for (int i = 0; i < n; i++)*/
+			/*[>fscanf();<]*/
+			/*// TODO*/
+/*}*/
+
+
+void array_flush(double *v, int size)
+{
 	for(long int i=0; i < size; i++)
 		_mm_clflush(v + i);
 }
@@ -122,7 +125,8 @@ void solve2x2(double *restrict A, double *restrict rhs, double *restrict x){
 
 }
 
-void verify_dir(const char *dir){
+void verify_dir(const char *dir)
+{
 	struct stat sb;
 	int rval = stat(dir, &sb);
 	if((rval == -1) && (errno == ENOENT)){
@@ -136,7 +140,8 @@ static std::streambuf *sbuf_backup;
 static std::ofstream ofile;
 static int linkcout_state = 0;
 
-void link_cout(const char *fname){
+void link_cout(const char *fname)
+{
 	assrt(linkcout_state == 0);
 	linkcout_state = 1;
 
@@ -147,7 +152,8 @@ void link_cout(const char *fname){
 	assrt(cout.good());
 }
 
-void unlink_cout(){
+void unlink_cout()
+{
 	assrt(linkcout_state == 1);
 	linkcout_state = 0;
 
@@ -260,91 +266,4 @@ void print_matrix(const char* desc, const int m, const int n, double *a, int lda
 		printf("\n");
 	}
 }
-
 /******************************************************************************/
-/*
- * based on http://stackoverflow.com/questions/5252375/custom-c-assert-macro
- * works outside debug mode too
- */
-#define assrt(cond) \
-	do \
-{ \
-	if (!(cond)) \
-	{ \
-		fprintf(stderr,"ASSRT FAILED\n");		\
-		fprintf(stderr,"file: %s\n",__FILE__);		\
-		fprintf(stderr,"line: %d\n",__LINE__);		\
-		__builtin_trap();				\
-	} \
-} while(0)  
-/******************************************************************************/ 
-/*void array_in(const char* fname, const int m, const int n, double *restrict a,*/
-		/*const int lda)*/
-/*{*/
-	/*assrt(lda>=m); */
-
-	/*FILE *fp = fopen(fname,"r");*/
-	/*assrt(fp);*/
-	/*fprintf(stderr, "Import array from %s\n",fname);*/
-
-	/*for (int i = 0; i < m; i++)*/
-		/*for (int i = 0; i < n; i++)*/
-			/*[>fscanf();<]*/
-			/*// TODO*/
-/*}*/
-
-void array_out(const char* fname, const int m, const int n, double *restrict a,
-		const int lda)
-{
-	assrt(lda>=m);
-
-	FILE *fp;
-	if(fname == NULL) {
-		fp = stdout;
-		fprintf(stderr, "Export array to STDOUT\n");
-	}
-	else {
-		fp = fopen(fname, "w");
-		fprintf(stderr, "Export array to %s\n",fname);
-	}
-
-	for(int i=0; i < m; i++){
-		for(int j=0; j < n; j++)
-			fprintf(fp, "%f  ", a[i+j*lda]);
-		fprintf(fp, "\n");
-	}
-
-	if(fname != NULL){
-		int rval = fclose(fp);
-		assrt(rval !=  -1);
-	}
-}
-
-void array_flush(double *v, int size)
-{
-	for(long int i=0; i < size; i++)
-		_mm_clflush(v + i);
-}
-
-void solve2x2(double *restrict A, double *restrict rhs, double *restrict x)
-{
-	double a = A[0];
-	double b = A[2];
-	double c = A[1];
-	double d = A[3];
-	double det = a*d-b*c;
-	x[0] = (rhs[0]*d-rhs[1]*b)/det;
-	x[1] = (-rhs[0]*c+rhs[1]*a)/det;
-
-}
-
-void verify_dir(const char *dir)
-{
-	struct stat sb;
-	int rval = stat(dir, &sb);
-	if((rval == -1) && (errno == ENOENT)){
-		mkdir(dir, 0700);
-		stat(dir, &sb);
-	}
-	assrt(S_ISDIR(sb.st_mode));
-}
